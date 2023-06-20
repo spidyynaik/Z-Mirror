2041d29779f5d59a4b4c2d9bae256a7ba0b97a83
Spidy <128451864+spidyynaik@users.noreply.github.com>
2023-06-21 01:20:28 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -123,10 +123,10 @@ async def start(_, message):
     elif config_dict['DM_MODE']:
         start_string = 'Bot Started.\n' \
                        'Now I can send your stuff here.\n' \
-                       'Use me here: @Z_Mirror'
+                       'Made with ♥ By: @Mr_SPIDY'
     else:
         start_string = 'Sorry, you cant use me here!\n' \
-                       'Join @Z_Mirror to use me.\n' \
+                       'Contact @Mr_SPIDY to use me.\n' \
                        'Thank You'
     await sendMessage(message, start_string)
 
